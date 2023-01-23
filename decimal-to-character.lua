a5bb626ffcbd1d8641fddf0b299220547bcda7b3
Tick <103855043+TickingEmulator@users.noreply.github.com>
2023-01-22 20:29:17 -0500

    Update decimal-to-character.lua

--- decimal-to-character.lua
+++ decimal-to-character.lua
@@ -1,1 +1,3 @@
-
+local decimal = 64;
+-- Lua has a function called string.char(decimal) which converts decimal into character form. Lua searchs the ascii table for the decimal value and then returns the character version of the decimal value. 
+print(string.char(decimal)); -- @
