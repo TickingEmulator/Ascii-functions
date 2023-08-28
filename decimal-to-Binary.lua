79aad292e10acd2584765f677c594ae7686158ee
Tick <103855043+TickingEmulator@users.noreply.github.com>
2023-08-27 19:03:38 -0700

    Update decimal-to-Binary.lua

--- decimal-to-Binary.lua
+++ decimal-to-Binary.lua
@@ -1,23 +1,12 @@
-local binarySequence = {};
-local base2Table = {512, 256, 128, 64, 32, 16, 8, 4, 2, 1}; -- This table only goes up to 1023 meaning that's the limit for your decimal integer.
-local decimal = "0d665";
-function decimalToBinary(decimal)
-  local decimalParse = (decimal):gsub("0d","");
-  local decimalInteger = tonumber(decimalParse);
-  for i = 1,#base2Table do
-    if (decimalInteger >= base2Table[i]) then
-     binarySequence[i] = 1;
-     decimalInteger = decimalInteger - base2Table[i];
-    else
-      binarySequence[i] = 0;
-    end;
+local base2Table = {512, 256, 128, 64, 32, 16, 8, 4, 2, 1}; -- This could be a lot more simpler with integer division but this gives you more of a breakdown proccess.
+local decimal = 665;
+local binary = "";
+for i = 1,#base2Table do
+  if (decimal >= base2Table[i]) then
+    binary = binary .. "1";
+    decimal = decimal - base2Table[i];
+  else
+    binary = binary .. "0";
   end;
-  local combinedInteger = "";
-  for i2  = 1,#binarySequence do
-    combinedInteger = combinedInteger..binarySequence[i2];
-  end;
-  return combinedInteger;
 end;
-
-print(decimalToBinary(decimal));
-
+print(binary);
